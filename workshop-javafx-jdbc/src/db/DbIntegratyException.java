5c6a94a795007468156a4db03fc4aca5aa0c305e
Thais Cardoso <thaiscardoso.ms@gmail.com>
2020-03-14 02:49:10 -0300

    Projeto até aqui

---
+++ workshop-javafx-jdbc/src/db/DbIntegratyException.java
@@ -0,0 +1,10 @@
+package db;
+
+public class DbIntegratyException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DbIntegratyException(String msg)	{
+		super(msg);
+	}
+}
